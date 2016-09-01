9580e568fe3e01d2fbccb05676df3b05362e4f4a
Dmitry <theash812@gmail.com>
2016-09-01 21:46:04 +0300

    добавил анимацию

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+var link = document.querySelector("#dropfilter");
+var popup = document.querySelector(".drop-form");
+var close = popup.querySelector(".drop-form");
+
+link.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.toggle("drop-form-show");
+	});
